42364767b2cbf5382c082c40f9bba49df215ed60
imiron13 <imironkbr@gmail.com>
2024-07-25 00:15:04 +0200

    Fix stdout retarget

--- Core/Src/user_main.cpp
+++ Core/Src/user_main.cpp
@@ -7,11 +7,9 @@ extern UART_HandleTypeDef huart1;
 
 extern "C" int _write(int file, char *ptr, int len)
 {
-  for (int i = 0; i < len ; i++)
-  {
-	  HAL_UART_Transmit(&huart1, (const uint8_t *)ptr, len, 100);
-  }
-  return len;
+	HAL_UART_Transmit(&huart1, (const uint8_t *)ptr, len, 100);
+
+	return len;
 }
 
 int user_main()
