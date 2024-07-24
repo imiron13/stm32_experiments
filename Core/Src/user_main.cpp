66b8a6de259b02f6a1e9ed059cd71290a4718c72
imiron13 <imironkbr@gmail.com>
2024-07-25 00:01:37 +0200

    Retarget stdio to UART1, printf demo

--- Core/Src/user_main.cpp
+++ Core/Src/user_main.cpp
@@ -5,10 +5,20 @@
 
 extern UART_HandleTypeDef huart1;
 
+extern "C" int _write(int file, char *ptr, int len)
+{
+  for (int i = 0; i < len ; i++)
+  {
+	  HAL_UART_Transmit(&huart1, (const uint8_t *)ptr, len, 100);
+  }
+  return len;
+}
+
 int user_main()
 {
     while (1)
     {
+    	printf("STM32 Experiments FW\n");
     }
     
     return 0;
