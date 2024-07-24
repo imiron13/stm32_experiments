e4716cc0181a0a3865860cc7d1a49ba516faa24e
imiron13 <imironkbr@gmail.com>
2024-07-24 22:39:09 +0200

    Add user_main.cpp/h in order to use C++ code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "user_main.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -98,7 +98,7 @@ int main(void)
   MX_RTC_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-
+  user_main();
   /* USER CODE END 2 */
 
   /* Infinite loop */
---
+++ Core/Src/user_main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "user_main.h"
+#include "main.h"
+
+extern UART_HandleTypeDef huart1;
+
+int user_main()
+{
+    while (1)
+    {
+    }
+    
+    return 0;
+}
